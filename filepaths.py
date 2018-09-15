9bddd4fdea20a03b424d4736c76241e94bcabecd
vandyand <vandyand@gmail.com>
2018-09-14 21:56:22 -0400

    updated filepaths.py so it works with server

--- filepaths.py
+++ filepaths.py
@@ -5,11 +5,17 @@ Created on Fri Sep 14 20:51:21 2018
 @author: dell
 """
 
+import os
+
 def filepaths():
-    file = open('filepaths.txt','r')
-    filepaths = file.read().split('\n')
-    filepaths = {t[0]:t[1] for t in [ i.split('=') for i in filepaths ]}
-    file.close()
+    with open('filepaths.txt','r') as file:
+        filepaths = file.read().split('\n')
+        filepaths = {t[0]:t[1] for t in [ i.split('=') for i in filepaths ]}
+        filepaths['DataHub'] = filepaths['DataHub1'] if os.environ['COMPUTERNAME']=='DELL-PC' else filepaths['DataHub2']
+        filepaths.pop('DataHub1',None)
+        filepaths.pop('DataHub2',None)
+        filepaths['Back'] = filepaths['DataHub']+'\\Back'
+        filepaths['Live'] = filepaths['DataHub']+'\\Live'
     return filepaths
     
     
